ce0a414be04b43c8c2857d20edfcea0f51fb03e9
deid01059 <deid01059@gmail.com>
2023-09-11 20:55:22 +0900

    09_11_php

--- php/self_tng/19x19.php
+++ php/self_tng/19x19.php
@@ -38,29 +38,29 @@
 
 // 19단중 9단까지는 홀수번째곱하기 생략 10단 부터 짝수단 생략 그리고 짝수번째곱하기 생략
 
-for( $num = 1; $num <= 9; $num++ ){
-    if ($num <= 10 ){
-        echo $num,"단\n";
-        for( $sec_num = 1; $sec_num <= 19; $sec_num++){
-            if ($sec_num % 2 === 1 ){
-                continue;
-            }
-            echo "$num x $sec_num =",$sec_num*$num,"\n";
-        }
-    }
-}  
-for( $num = 10; $num <= 19; $num++ ){
-    if ($num >= 10 && $num % 2 === 0){
-        continue;
-    }    
-    echo $num,"단\n";
-        for( $sec_num = 1; $sec_num <= 19; $sec_num++){
-            if ( $sec_num % 2 === 0){
-                continue;
-            }    
-            echo "$num x $sec_num =",$sec_num*$num,"\n";
-        }
-}
+// for( $num = 1; $num <= 9; $num++ ){
+//     if ($num <= 10 ){
+//         echo $num,"단\n";
+//         for( $sec_num = 1; $sec_num <= 19; $sec_num++){
+//             if ($sec_num % 2 === 1 ){
+//                 continue;
+//             }
+//             echo "$num x $sec_num =",$sec_num*$num,"\n";
+//         }
+//     }
+// }  
+// for( $num = 10; $num <= 19; $num++ ){
+//     if ($num >= 10 && $num % 2 === 0){
+//         continue;
+//     }    
+//     echo $num,"단\n";
+//         for( $sec_num = 1; $sec_num <= 19; $sec_num++){
+//             if ( $sec_num % 2 === 0){
+//                 continue;
+//             }    
+//             echo "$num x $sec_num =",$sec_num*$num,"\n";
+//         }
+// }
 
 
 // 2에서 +3의 배수단들만 나타내고 그중 짝수번째 단은 홀수곱하기항목 생략
