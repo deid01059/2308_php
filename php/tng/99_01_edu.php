690b23b49eb9b1d27dab9ccdcd507ce24bc6048c
deid01059 <deid01059@gmail.com>
2023-09-13 20:53:23 +0900

    0913_php

---
+++ php/tng/99_01_edu.php
@@ -0,0 +1,61 @@
+<?php
+
+// if문이용 : 90이면 수, 80이면 우, 그 외는 "노력"
+
+// $grade = 87;
+
+// if($grade === 90){
+// 	echo "수";
+// }
+// else if($grade === 80){
+// 	echo "우";
+// }
+// else {
+// 	echo "노력";
+// }
+
+
+// while문 이용 : 구구단 7단
+// $i = 1 ;
+
+// while (true) {
+// 	$num = $i * 7;
+// 	echo " 7 X {$i} = {$num} \n";
+// 	$i++;
+// 	if($i === 10){
+// 		break;
+// 	}
+// }
+
+
+
+$arr = [1, 2, 3];
+
+$arr1 = [
+	"key1" => "val1"
+	,"key2" => [
+				"key3" => "val3"
+				,"key4" => "val4"
+	]
+];
+
+
+// echo $arr[2], $arr1["key2"];
+
+
+
+// foreach( $arr1["key2"] as $val) {
+// 	echo $val,"\n";
+// }
+
+var_dump ($arr);
+// 데이터타입 위치 구조 등 모든정보가 다출력
+
+print_r ($arr);
+// 안에 내용만
+
+
+define("MSG",)
+
+
+?>
