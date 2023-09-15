c79e5d75a24cefb35cebbaf5770d94a2b73221db
deid01059 <deid01059@gamil.com>
2023-09-15 16:46:54 +0900

    0915_php

--- php/self_tng/baseball.php
+++ php/self_tng/baseball.php
@@ -6,24 +6,26 @@ $base_no_3 = rand(1,9);
 
 
 while(true){
-    $str = " %u 스트라이크";
-    $bal = " %u 볼";
-
+    $str = 0;
     $user_no = trim(fgets(STDIN));
     $com = [$base_no_1,$base_no_2,$base_no_3];
+    if($str <= 2){    
         if($com[0] == $user_no[0]){
             $str + 1;
         }
-        if($com[1] == $user_no[1]){
+        else if($com[1] == $user_no[1]){
             $str + 1;
         }
-        if($com[1] == $user_no[1]){
+        else if($com[2] == $user_no[2]){
             $str + 1;
         }
-        else if($com[0] == $user_no[0]){
-            $str + 1;
-        }
-
+        
+    }
+    if($str == 3){
+        echo "$str 스트라이크!!! 아웃 you win";
+        break;
+    }    
+    echo "$str 스트라이크 $ball 볼";
 
 
 
