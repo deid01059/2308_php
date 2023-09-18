a548950a30c6e5d9d99423929acfa526ccda27b3
deid01059 <deid01059@gamil.com>
2023-09-18 18:14:23 +0900

    0918_php

--- php/self_tng/0918_homework.php
+++ php/self_tng/0918_homework.php
@@ -14,7 +14,6 @@ $sql =
 	." ,last_name "
 	." ,gender "
 	." ,hire_date "
-
     ." ) "
 ." VALUES ( "
     ." :emp_no "
@@ -26,12 +25,12 @@ $sql =
     ." ); ";
 
 $arr_ps = [
-    " :emp_no " => 500001
-    ," :emp_birth_date " => 19981023
-    ," :emp_first_name " => "junghoon"
-    ," :emp_last_name " => "choi"
-    ," :emp_gender " => "M"
-    ," :emp_hire_date " => "20230918"
+    ":emp_no" => 500001
+    ,":emp_birth_date" => 19981023
+    ,":emp_first_name" => "junghoon"
+    ,":emp_last_name" => "choi"
+    ,":emp_gender" => "M"
+    ,":emp_hire_date" => "20230918"
 ];
 
 $stmt = $conn->prepare($sql);
