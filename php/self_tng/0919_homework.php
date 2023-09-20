ebd2f99f70c5b18bb4f5da29a8163952aff4223f
deid01059 <deid01059@gamil.com>
2023-09-20 17:33:05 +0900

    0920_php

--- php/self_tng/0919_homework.php
+++ php/self_tng/0919_homework.php
@@ -4,61 +4,64 @@ require_once("../ex/04_107_fnc_db_connect.php");
 $conn = null;
 my_db_conn($conn);
 
-$sql = " SELECT "
-        ." * "
-        ." FROM "
-        ." employees emp "
-        ." WHERE emp.emp_no "
-        ." NOT IN "
-        ." ( SELECT ti.emp_no FROM titles ti); "
-        ;
+try{
+    $sql_sel = " SELECT "
+            ." emp.emp_no "
+            ." FROM "
+            ." employees emp "
+            ." WHERE NOT EXISTS "
+            ." ( "
+            ." SELECT 1 "
+            ." FROM "
+            ." titles ti "
+            ." WHERE "
+            ." emp.emp_no = ti.emp_no "
+            ." ); "
+            ;
 
-// prepared Statement 셋팅
-$arr_ps = [
-];
-
-
-$stmt = $conn->prepare($sql);
-$stmt->execute($arr_ps);
-$resert = $stmt->fetchall();
-
-print_r($resert);
-
-
-$sql = " INSERT INTO "
-." titles ( "
-." emp_no "
-." ,title "
-." ,from_date "
-." ,to_date "
-." ) "
-." VALUES ( "
-." :emp_no "
-." ,:title "
-." ,now() "
-." ,:to_date "
-." ); ";
-$stmt = $conn->prepare($sql);
-
-$a = 0;
-foreach($resert as $item){
-    $a = $item["emp_no"];
-
+    // prepared Statement 셋팅
     $arr_ps = [
-        ":emp_no" => $a
-        ,":title" => "green"
-        ,":to_date" => 99990101
     ];
-    
-    $result1 = $stmt->execute($arr_ps);
-    
-    var_dump($result1);
-    
+
+
+    $stmt = $conn->prepare($sql_sel);
+    $stmt->execute($arr_ps);
+    $resert = $stmt->fetchall();
+
+    print_r($resert);
+
+    $sql_in = " INSERT INTO "
+        ." titles ( "
+        ." emp_no "
+        ." ,title "
+        ." ,from_date "
+        ." ,to_date "
+        ." ) "
+        ." VALUES ( "
+        ." :emp_no "
+        ." ,:title "
+        ." ,now() "
+        ." ,99990101 "
+        ." ); "
+        ;
+    foreach($resert as $item){
+        $arr_ps = [
+                ":emp_no" => $item["emp_no"]
+                ,":title" => "green"
+        ];
+        $stmt = $conn->prepare($sql_in);
+        $result = $stmt->execute($arr_ps);
+        if(!$resert){
+            throw new Exception("insert error");
+        }
+    }    
+    $conn->commit();
+    echo "insert SUCCESS";
+} catch(Exception $e){
+    $conn->rollback();
+} finally {
+    $conn = null;
 }
-$conn -> commit();
-
-print_r($a)
-
 
 
 
